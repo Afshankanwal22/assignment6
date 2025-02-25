7f4d67ab78de4c367686465138709e4515f2aeb5
Afshan kanwal <kanwalafshan2244@gmail.com>
2025-02-26 02:36:09 +0500

    assignment6

---
+++ app.js
@@ -0,0 +1,63 @@
+//                                  CHAPTER 26 TO 30 
+
+// Qno:1
+// let num = prompt("Enetr your number");
+// let round = Math.round(num);
+// let floor = Math.floor(num);
+// let ceil = Math.ceil(num);
+// document.write(`Number: ${num} <br />Round of value: ${round} <br /> Floor value: ${floor} <br /> Ceil value: ${ceil}`)
+
+// Qno:2
+// let num = prompt("Enter your number");
+// let round = Math.round(num);
+// let floor = Math.floor(num);
+// let ceil = Math.ceil(num)
+// document.write(`Number:${num}<br />Round off value: ${round}<br /> Floor value: ${floor}<br />Ceil value: ${ceil}`);
+
+// Qno:3
+// let num = +prompt("Enter your number");
+// let absolute = Math.abs(num)
+// document.write(`The absolute value of: ${num} is ${absolute}`);
+
+// Qno:4
+// let random = Math.random()*6;
+// let floor= Math.floor(random);
+// let random1=Math.random()*7;
+// let floor1=Math.floor(random1)
+// document.write(`Random dice value: ${floor}<br />Random dice value: ${floor1}`);
+
+// Qno:5
+// let coinNum=Math.random()*3;
+// let floor=Math.floor(coinNum);
+// console.log(floor)
+// if(coinNum<2){
+//     document.write(`${floor}<br /> Random coin value: Tail`)
+
+// }
+// else{
+//     document.write(`${floor}<br /> Random coin value: Head`)
+// }
+
+// Qno:6
+// let random=Math.random()*100;
+// let floor=Math.floor(random);
+// console.log(floor)
+// document.write(`Random number between 1 and 100:${floor}`)
+
+// Qno:7
+// let weight=prompt("Enter your weight in kilograms");
+// let par=parseFloat(weight);
+// document.write(`The weight of user is ${par.toFixed(1)} kilograms`)
+
+// Qno:8
+// let random=Math.ceil((Math.random()*10));
+// console.log(random);
+
+// let userInput=prompt("Enter your number between 1 to 10");
+// if(userInput===random){
+//     alert("Congratulate; you entered the same number")
+// }
+// else{
+//     alert("Try again")
+// }
+
